d9222ebcc3a990fe55336f9b8f7f7a092708bca6
itayassor236 <itayasor236@gmail.com>
2022-04-06 17:19:36 +0300

    eat changes

--- ZOO2/src/zoo/ZooActions.java
+++ ZOO2/src/zoo/ZooActions.java
@@ -428,7 +428,7 @@ public class ZooActions {
 	   {
 		   MessageUtility.logBooleanFunction(array[j].getName(), "eat()",array[k].getName(), false);
 	   }
-	   System.out.print("finish");
+	  
    }
    
    }
